a2a80daafe0304f8b118ee4559696f0ab8a0a640
Abhi Panchami <abhijose.2020@gmail.com>
2024-02-14 16:08:28 +0530

    "commit message"

--- src/pages/PostDtail.jsx
+++ src/pages/PostDtail.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import PostAuthor from '../components/PostAuthor'
-import postDtailImage from '../assets/img1.jpg'
+import postdtailImage from '../assets/img1.jpg'
+
 export default function PostDtail() {
   return (
  <section>
@@ -15,6 +16,17 @@ export default function PostDtail() {
         </div>
       </div>
     </div>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+   <div className="postdtail-image">
+    <img src={postdtailImage} alt="" />
+   </div>
+   <div className="postdtail-para">
+   <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ex, qui praesentium fugiat deleniti consequuntur excepturi quasi ipsum debitis incidunt cupiditate iusto mollitia cumque ratione quos officiis quam pariatur soluta.
+   </p>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit el itur? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae pariatur sapiente maxime repellat atque voluptate reprehenderit quo explicabo qui veritatis! Impedit officia et obcaecati tempore, dolorem tempora hic possimus deserunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cumque saepe esse voluptatum. Exercitationem non voluptate nisi unde qui, explicabo reiciendis enim, soluta autem, id deleniti quam nesciunt dolorem veritatis.
+  </p>
+  </div>
   </div>
 
  </section>  )
