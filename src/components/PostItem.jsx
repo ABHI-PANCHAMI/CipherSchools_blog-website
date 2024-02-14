4dfcce42795ed4d01729342c127f0323269fab03
Abhi Panchami <abhijose.2020@gmail.com>
2024-02-14 15:37:26 +0530

    commit

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -2,7 +2,9 @@ import Posts from "./Posts"
 import { Link } from "react-router-dom"
 import PostAuthor from "./PostAuthor"
 export default function PostItem({Image, authorId, category, title, des, postId}) {
-  return (
+     const shortTitle =title.length>30?title.substring(0,30)+'...':title;
+     const shortDes =des.length> 140?des.substring(0,140)+'...':des;
+    return (
     <article className="post">
         <div className="post-image">
             <img src={Image} alt="" />
@@ -10,9 +12,9 @@ export default function PostItem({Image, authorId, category, title, des, postId}
             </div>
             <div className="post-content">
                 <Link to={'posts/${postId}'}>
-                    <h3>{title}</h3>
+                    <h3>{shortTitle}</h3>
                 </Link>
-                <p>{des}</p>
+                <p>{shortDes}</p>
                 <div className="post-footer">
                     <PostAuthor/>
                     <Link to={'/posts/categories/${category}'}className="btn btn-category">{category}</Link>
--- src/pages/PostDtail.jsx
+++ src/pages/PostDtail.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import PostAuthor from '../components/PostAuthor'
 export default function PostDtail() {
   return (
-    <div>PostDtail</div>
-  )
+ <section>
+  <div className='container PostDtail-container'>
+    <div className='postdtail-top'>
+      <PostAuthor/>
+      <div className="postdtail-buttons">
+        <div className="post-dtail-buttons"></div>
+      </div>
+    </div>
+  </div>
+
+ </section>  )
 }
