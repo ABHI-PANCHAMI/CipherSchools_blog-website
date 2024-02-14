dfcc12924b4b765c125e22f2ca0e98863695f03c
Abhi Panchami <abhijose.2020@gmail.com>
2024-02-14 16:45:18 +0530

    committed

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,12 +5,14 @@ export default function Posts() {
   const [posts,setPosts] = useState(DummyPosts)
     return (
     <section className="posts">
+      {
+        posts.length > 0 ? 
         <div className="container posts-container">
         {
             posts.map(({id,Image, authorId, category, title, des},index) => <PostItem key={index} Image={Image} category={category} authorId={authorId} title={title} des={des} postId={id}/>)
         }
-
-        </div>
+ 
+        </div> : <h2 className="error-center">No posts found</h2>}
     </section>
   )
 }
--- src/pages/PostDtail.jsx
+++ src/pages/PostDtail.jsx
@@ -9,11 +9,9 @@ export default function PostDtail() {
     <div className='postdtail-top'>
       <PostAuthor/>
       <div className="postdtail-buttons">
-        <div className="post-dtail-buttons">
           <Link to={'/posts/werwer/edit'} className='btn btn-sm btn-primary' >Edit</Link>
           <Link to={'/posts/werwer/delete'} className='btn btn-sm btn-danger' >Delete</Link>
-          
-        </div>
+        
       </div>
     </div>
     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
@@ -25,6 +23,12 @@ export default function PostDtail() {
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ex, qui praesentium fugiat deleniti consequuntur excepturi quasi ipsum debitis incidunt cupiditate iusto mollitia cumque ratione quos officiis quam pariatur soluta.
    </p>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit el itur? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae pariatur sapiente maxime repellat atque voluptate reprehenderit quo explicabo qui veritatis! Impedit officia et obcaecati tempore, dolorem tempora hic possimus deserunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cumque saepe esse voluptatum. Exercitationem non voluptate nisi unde qui, explicabo reiciendis enim, soluta autem, id deleniti quam nesciunt dolorem veritatis.
+  </p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit el itur? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae pariatur sapiente maxime repellat atque voluptate reprehenderit quo explicabo qui veritatis! Impedit officia et obcaecati tempore, dolorem tempora hic possimus deserunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cumque saepe esse voluptatum. Exercitationem non voluptate nisi unde qui, explicabo reiciendis enim, soluta autem, id deleniti quam nesciunt dolorem veritatis.
+  </p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit el itur? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae pariatur sapiente maxime repellat atque voluptate reprehenderit quo explicabo qui veritatis! Impedit officia et obcaecati tempore, dolorem tempora hic possimus deserunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cumque saepe esse voluptatum. Exercitationem non voluptate nisi unde qui, explicabo reiciendis enim, soluta autem, id deleniti quam nesciunt dolorem veritatis.
+  </p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit el itur? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae pariatur sapiente maxime repellat atque voluptate reprehenderit quo explicabo qui veritatis! Impedit officia et obcaecati tempore, dolorem tempora hic possimus deserunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime cumque saepe esse voluptatum. Exercitationem non voluptate nisi unde qui, explicabo reiciendis enim, soluta autem, id deleniti quam nesciunt dolorem veritatis.
   </p>
   </div>
   </div>
